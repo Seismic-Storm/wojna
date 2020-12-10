006b08fbe5b8d29fc7c78314030a45156a54a483
Seismic-Storm <67949839+Seismic-Storm@users.noreply.github.com>
2020-12-10 15:47:51 +0100

    Create wojna_hela.py

---
+++ wojna_hela.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""autor: Helena Ciechowska
+10.12.2020"""
+
+class Deck:
+    pass
+    
+    
+class Hand:
+    pass
+    
+    
+class Table:
+    pass
+    
+    
+if __name__ == '__main__':
+    pass
